78e8ef147889d02e087987846fd7d8fc3b95ca60
yalh76 <yalh@yahoo.com>
2020-04-01 03:46:31 +0200

    Removing  php7.3-uploadprogress

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="postgresql curl libzip-dev"
-extra_pkg_dependencies="php7.3-fpm php7.3-cli php7.3-gd php7.3-mysql php7.3-xml php7.3-ldap php7.3-mbstring php7.3-uploadprogress php7.3-pgsql"
+extra_pkg_dependencies="php7.3-fpm php7.3-cli php7.3-gd php7.3-mysql php7.3-xml php7.3-ldap php7.3-mbstring php7.3-pgsql"
 
 #=================================================
 # PERSONAL HELPERS
