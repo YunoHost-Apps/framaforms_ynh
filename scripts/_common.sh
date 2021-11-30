25e934183abf63997e370218ec1bdfb999b610b6
ericgaspar <junk.eg@free.fr>
2021-11-30 22:07:44 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,7 @@
 
 YNH_PHP_VERSION="7.3"
 
-pkg_dependencies="postgresql curl libzip-dev php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pgsql"
-
-#YNH_COMPOSER_VERSION="1.10.17"
+pkg_dependencies="postgresql libzip-dev php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-domphp${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pgsql"
 
 #=================================================
 # PERSONAL HELPERS
