0fd09b484937c845a045d0415a3829a04a07ed6c
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-02 19:33:34 +0200

    v2

--- doc/DISCLAIMER.md
+++
@@ -1,3 +0,0 @@
-## Configuration
-
-You can modify some configurations using the Framaforms administration panel. You can log in with the admin user and the password you gave during installation.
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="7.3"
-
-#REMOVEME? pkg_dependencies="postgresql curl libzip-dev php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pgsql"
-
-YNH_COMPOSER_VERSION="1.10.17"
+YNH_COMPOSER_VERSION="2.5.7"
 
 #=================================================
 # PERSONAL HELPERS
