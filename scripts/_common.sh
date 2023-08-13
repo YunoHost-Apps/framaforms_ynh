e27d4428e799518f593b3494dd54c5fa190a5e34
lapineige <lapineige@users.noreply.github.com>
2023-08-13 11:08:31 +0200

    Remove all RemoveMe
    Brute force, need to clean and check it…

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="7.3"
 
-#REMOVEME? pkg_dependencies="postgresql curl libzip-dev php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pgsql"
+
+
 
 YNH_COMPOSER_VERSION="1.10.17"
 
