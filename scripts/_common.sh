ef6df4f2ca8505972d4bb61a119c4817bfa958bb
eric_G <46165813+ericgaspar@users.noreply.github.com>
2026-01-30 10:56:57 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-composer_version="1.10.17"
-
 _ynh_exec_with_drush_php() {
     ynh_hide_warnings ynh_exec_as_app \
         env PATH="$install_dir/.composer/vendor/bin:$PATH" \
